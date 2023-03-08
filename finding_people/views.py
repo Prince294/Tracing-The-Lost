7b0846780c7012fe33bf0228d9399cee4218715a
Prince294 <72266126+Prince294@users.noreply.github.com>
2023-03-08 10:06:00 +0530

    remove unnecessary files

--- finding_people/views.py
+++ finding_people/views.py
@@ -549,10 +549,17 @@ def ExtractFaces(request):
 @api_view(['GET','POST'])
 def AadharDetail(request):
     if request.method == 'GET':
-        modelAadhar = AadharData.objects.all()
-        serializerAadhar = AadharDataSerializer(modelAadhar,many=True).data
-        
-        return Response(serializerAadhar,status.HTTP_200_OK)
+        clientData = request.data
+        aadhar_num = clientData.get('aadhar_number')
+        try:
+            serializerAadhar = AadharDataSerializer(AadharData.objects.get(aadhar_number=aadhar_num)).data
+            return Response(serializerAadhar,status.HTTP_200_OK)
+        except:
+            res = {
+                'status':'error',
+                'message':'No Aadhar Detail Found!'
+            }
+            return responseMaker(res,status.HTTP_404_NOT_FOUND)
         
             
     if request.method == 'POST':
